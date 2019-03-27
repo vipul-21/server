b356c0c28649ea7760011d7956b69ade106e6e40
vipul <vipul21sept@gmail.com>
2019-03-27 01:38:58 -0700

    Added router chi !

--- golang/server.go
+++ golang/server.go
@@ -1,15 +1,27 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello world!")
-	})
-
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Fprintf(w, "Hello world!")
+	// })
+	//
 	// http.Handle("/", func())
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", buildRouter())
+}
+
+func buildRouter() http.Handler {
+	router := chi.NewRouter()
+	router.Get("/test", func(wrt http.ResponseWriter, rd *http.Request) {
+		wrt.Write([]byte("welcome"))
+	})
+	router.Get("/", func(wrt http.ResponseWriter, rd *http.Request) {
+		wrt.Write([]byte("first page"))
+	})
+	return router
 }
